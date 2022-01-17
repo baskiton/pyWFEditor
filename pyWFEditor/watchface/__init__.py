233c406e0a8825a8b8013474493a64a7b00e826c
baskiton <baskiton@mail.ru>
2022-01-17 23:50:46 +0700

    Add UUIH support exclude compressed files. Apparently my implementation of quicklz is not working correctly.

--- pyWFEditor/watchface/__init__.py
+++ pyWFEditor/watchface/__init__.py
@@ -1,4 +1,5 @@
 import io
+import pprint
 
 from typing import BinaryIO
 
@@ -6,6 +7,7 @@ import wx
 
 from . import elements, errors
 from .. import utils
+from ..utils import quicklz
 from .wfimage import WFImage
 from .wfparameter import WFParameter
 
@@ -21,18 +23,33 @@ class WFHeader:
         H_SIGN: 0x0B,
         U_SIGN: 0x04,
     }
+
     H_HDR_LEN = {
         0xFF: 40,
         0x06: 64,
     }
+    H_DEV_ID_LOC = 0x10
+
+    U_VER_NORMAL = 0x01
+    U_VER_COMPRESS = 0x02
+    U_COMPRESS_START = 0x28
+    U_HDR_LEN = 87
 
     def __init__(self, buf: BinaryIO):
         self.unknown0 = self.unknown1 = self.hdr_params_size = self.device_id = self.version = -1
 
         buf.seek(0)
+        if utils.io2int(buf, 1) == 0x4F:   # qlz packed
+            unc = quicklz.qlz_stream_decompress(buf)
+            buf.seek(0)
+            buf.write(unc)
+            wx.LogDebug('file compressed. Decompress OK')
+
+        buf.seek(0)
         s_len = self.SIGN_LEN.get(buf.read(1))
         if s_len is None:
             raise errors.WFFileFormatError()
+
         buf.seek(0)
         sign = buf.read(s_len)
         if sign == self.H_SIGN:
@@ -48,7 +65,7 @@ class WFHeader:
         hdr_len = self.H_HDR_LEN.get(self.version)
         if not hdr_len:
             raise errors.WFFileFormatError()
-        buf.seek(0x10)
+        buf.seek(self.H_DEV_ID_LOC)
         self.device_id = utils.io2int(buf, 1)
         buf.seek(hdr_len - 12)
         self.unknown1 = utils.io2int(buf, 4, signed=True)
@@ -56,7 +73,25 @@ class WFHeader:
         self.hdr_params_size = utils.io2int(buf, 4, signed=True)
 
     def uihh_build(self, buf):
-        raise NotImplementedError()
+        buf.seek(self.VER_POS[self.U_SIGN])
+        self.version = utils.io2int(buf, 2)
+        if self.version == self.U_VER_COMPRESS:
+            raise errors.WFFileUnsupportedFormat()
+            # buf.seek(0)
+            # buf.seek(self.U_COMPRESS_START)
+            # unc = quicklz.qlz_stream_decompress(io.BytesIO(buf.read()))
+            # buf.seek(self.U_COMPRESS_START)
+            # buf.write(unc)
+
+        # TODO: find device id
+        buf.seek(self.U_HDR_LEN - 12)
+        if self.version == self.U_VER_NORMAL:
+            v = 'unknown1', 'unknown0', 'hdr_params_size'
+        else:   # U_VER_COMPRESS
+            v = 'unknown1', 'hdr_params_size', 'unknown0'
+            buf.seek(1, io.SEEK_CUR)
+        for i in v:
+            setattr(self, i, utils.io2int(buf, 4, signed=True))
 
 
 class WatchFace:
@@ -71,14 +106,7 @@ class WatchFace:
     def from_file(cls, fn):
         wx.LogMessage(f'Reading {fn}')
 
-        f = open(fn, 'rb')
-        f.seek(0, io.SEEK_END)
-        fsz = f.tell()
-        f.seek(0, io.SEEK_SET)
-
-        if utils.io2int(f, 1) == 0x4F:   # qlz packed
-            f = io.BytesIO(utils.quicklz.qlz_stream_decompress(f))
-            wx.LogDebug('file compressed. Decompress OK')
+        f = io.BytesIO(open(fn, 'rb').read())
 
         hdr = WFHeader(f)
         hdr_params = io.BytesIO(f.read(hdr.hdr_params_size))
@@ -87,14 +115,34 @@ class WatchFace:
         elems = {1: main_param}
 
         param_loc = WFParameter.read_list(hdr_params, hdr.hdr_params_size)
+        hdr_params.close()
         params = io.BytesIO(f.read(main_param.table_len))
 
-        offsets = list(utils.io2int(f, 4) for _ in range(main_param.images_cnt))
-        offsets.sort()
+        cur = f.tell()
+        f.seek(0, io.SEEK_END)
+        fsz = f.tell()
+        f.seek(cur)
+
+        offsets = []
+        for _ in range(main_param.images_cnt):
+            v = utils.io2int(f, 4, signed=True)
+            if v > fsz or v < 0:
+                f.seek(-4, io.SEEK_CUR)
+                break
+            offsets.append(v)
+        # offsets = list(utils.io2int(f, 4) for _ in range(main_param.images_cnt))
+        # offsets.sort()
         offsets.append(fsz)
 
-        # images = p = None
-        images = cls._images_collect(f, offsets)
+        wx.LogDebug(str(main_param))
+        wx.LogDebug(str(hdr.hdr_params_size))
+        _s = io.StringIO()
+        pprint.pprint(param_loc, width=3, stream=_s)
+        wx.LogDebug('\n' + _s.getvalue())
+        # wx.LogDebug(offsets, width=3)
+
+        images = p = None
+        # images = cls._images_collect(f, offsets)
         # elems.update(cls._elements_collect(params, param_loc))
         p = cls._elements_collect(params, param_loc)
 
